bc8af6eac7eb19524056cde2e79942bbd7dad6c7
Guilherme Coelho Vieira <gui130504.gv@gmail.com>
2024-03-20 13:42:26 +0100

    Capacity VF WIP
    Capacity version finale en cours de validation / finalisation.

--- Pokonsole/Source/Pokemon/Capacity.cs
+++ Pokonsole/Source/Pokemon/Capacity.cs
@@ -2,45 +2,66 @@
 {
     internal class Capacity
     {
-
-        enum ATTACK_TYPE
+        //CONSTRUTEUR
+        public Capacity(string name, POKEMON_TYPE type, int power, int accuracy /*POKEMON_STATUS effet = POKEMON_STATUS.NORMAL*/) 
         {
-            None = 0,
-            ATTACKBUFFING,//_m_capacityDamage 15%
-            DEFENSEBUFFING,
-            SPECIALBUFFING,
-            SPEEDBUFFING,//_m_capacityDefense 15%
-            STATUSBUFFING, //_m_statusEffect POISON
-            TOTAL_ATTACK_TYPE
-        }
-        public Capacity() 
-        {
+            Name = name;
+            Type = type;
+            Power = power;
+            Accuracy = accuracy;
+            //Status = effet; //? Si le pokémon est déjà POISONED et qu'il prends une attaque Charge de status NORMAL, le pokemon reste POISONED ou deviens NORMAL ? 
         }
 
-        public void AddAttackDamage(Pokemon target) 
-        {
-
-            target.Attack1 = target.Attack1 + m_capacityDamage;
+        public Capacity(string name, POKEMON_TYPE type, int power, int accuracy, POKEMON_STATUS effet) 
+        { 
+            Name = name;
+            Type = type;
+            Power = power;
+            Accuracy = accuracy;
+            Status = effet;
         }
 
-        public void AddDefense(Pokemon target) 
+        //METHODES
+        public void AttackBuff(Pokemon target)
         {
-            target.Defense = target.Defense + m_capacityDefense;
+            target.Attack1 = target.Attack1 + Power;
         }
-        public void SetStatusEffect(Pokemon target, POKEMON_STATUS status) 
+        public void DefenseBuff(Pokemon target)
         {
-            target.State = status;
+            target.Defense = target.Defense + Power;
+        }
+        public void AttackSpeBuff(Pokemon target)
+        {
+            target.AttackSpe = target.AttackSpe + Power;
+        }
+        public void DefenseSpeBuff(Pokemon target)
+        {
+            target.DefenseSpe = target.DefenseSpe + Power;
+        }
+        public void SpeedBuff(Pokemon target)
+        {
+            target.Speed = target.Speed + Power;
         }
 
-        private float Power = 1.0f;
-        private float m_capacityDamage = 0f;
-        private float m_capacityDefense = 0f;
-        private POKEMON_STATUS m_statusEffect = POKEMON_STATUS.NORMAL;
-        private float m_capacityCrit = 0f;
+        //METHODES GETTER / SETTER
+        public string Name { get => _name; set => _name = value; }
+        public POKEMON_TYPE Type { get => _type; set => _type = value; }
+        public int Power { get => _power; set => _power = value; }
+        public int Accuracy { get => _accuracy; set => _accuracy = value; }
+        public POKEMON_STATUS Status {get => _status; set => _status = value; }
 
-        public float _Power { get { return Power; } set { Power = value; } }
-        private string _Name;
-        private float _Type;
-
+        //VARIABLES PRIVEES
+        private string _name;
+        private POKEMON_TYPE _type;
+        private int _power;
+        private int _accuracy;
+        private POKEMON_STATUS _status;
     }
 }
+
+
+/// Name : Charge   | Danse Lames } Buff l'attaque du lanceur de Power % et avec un taux de réusite de Accuracy %   | Poison } Effectue Power% de dégat sur 3 tours
+/// Type : Normal   | Normal                                                                                        | Poison 
+/// Power : 40      | -- (20)                                                                                       | 50
+/// Accuracy : 100  | -- (100)                                                                                      | 100
+///                                                                                                                 | STATUS : POISONED
--- Pokonsole/Source/Pokemon/Pokemon.cs
+++ Pokonsole/Source/Pokemon/Pokemon.cs
@@ -55,26 +55,34 @@ namespace Pokonsole.Source.Pokemon
         public void OnExit() { }
         public void OnCapture()
         {
-            _captured = true;
+            _isCaptured = true;
         }
-        private float _attack;
-        private float _healthPoint;
-        private float _defense;
-        private float _speed;
-        private int _level;
-        private string _name = "";
-        private bool _captured = false;
-        private POKEMON_STATUS _status = POKEMON_STATUS.NORMAL;
-        private POKEMON_TYPE _type; 
 
+        private string _name = "";
+        private int _level;
+
+        private float _healthPoint;
+        private float _attack;
+        private float _defense;
+        private float _attackSpe;
+        private float _defenseSpe;
+        private float _speed;
+
+        private bool _isCaptured = false;
+        private POKEMON_STATUS _status = POKEMON_STATUS.NORMAL;
+        private POKEMON_TYPE _type;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Level { get => _level; set => _level = value; }
+
+        public float HealthPoint { get => _healthPoint; set => _healthPoint = value; }
         public float Attack1 { get => _attack; set => _attack = value; }
         public float Defense { get => _defense; set => _defense = value; }
-        public float HealthPoint { get => _healthPoint; set => _healthPoint = value; }
+        public float AttackSpe { get => _attackSpe; set => _attackSpe = value; }
+        public float DefenseSpe { get => _defenseSpe; set => _defenseSpe = value; }
         public float Speed { get => _speed; set => _speed = value; }
-        public int Level { get => _level; set => _level = value; }
-        public string Name { get => _name; set => _name = value; }
-        public bool Captured { get => _captured; set => _captured = value; }
 
+        public bool IsCaptured { get => _isCaptured; set => _isCaptured = value; }
         public POKEMON_STATUS State { get => _status; set => _status = value; }
         public POKEMON_TYPE Type { get => _type; set => _type = value; }
 
